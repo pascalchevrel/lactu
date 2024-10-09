b5c43343f9157120d2f05fff5c3732f4a5b94baa
Pascal Chevrel <pascal@mozilla.com>
2024-10-09 15:09:15 +0200

    Styling nits

--- admin/template.php
+++ admin/template.php
@@ -1,22 +1,17 @@
-<?php if(!isset($admin_access)) return; ?>
+<?php if (! isset($admin_access)) return; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <title>
-
 <?php
     echo _g('Administration') . ' — ';
     printf(_g('moonmoon %s', '%s is the version number for moonmoon, this string appears as the tab title in the admin panel'), $moon_version);
 ?>
     </title>
     <link rel="stylesheet" media="screen" type="text/css" href="default.css">
-<!--[if lte IE 9]>
-<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
 
     <?=@$header_extra ?: ''; ?>
-
 </head>
 
 <body id="<?=@$page_id ?: ''; ?>">
@@ -26,7 +21,7 @@
             <p><a href="../"><?=_g('Back to main page')?></a></p>
         </header>
 
-        <?php if($admin_access == 1) : ?>
+        <?php if ($admin_access == 1) : ?>
 
         <p class="logout"><a href="logout.php"><?=_g('Logout')?></a></p>
         <nav>
@@ -37,8 +32,6 @@
         </nav>
 
         <?php endif; ?>
-
-
 
         <div id="content">
 
